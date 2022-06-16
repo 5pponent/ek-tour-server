ebbd3d02edbb91f4e95945d11d1a6bfe2889641f
Seong Heung Bae <mopil1102@gmail.com>
2022-06-16 14:08:16 +0900

    initial commit

---
+++ src/main/java/renewal/ektour/EkTourApplication.java
@@ -0,0 +1,13 @@
+package renewal.ektour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/renewal/ektour/EkTourApplicationTests.java
@@ -0,0 +1,13 @@
+package renewal.ektour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
