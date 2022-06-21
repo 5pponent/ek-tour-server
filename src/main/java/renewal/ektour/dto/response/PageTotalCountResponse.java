dbddd1b7fef7d68511808ac7d6e3adf189a4b853
Seong Heung Bae <mopil1102@gmail.com>
2022-06-21 17:28:00 +0900

    최종 견적 엔티티 분리

---
+++ src/main/java/renewal/ektour/dto/response/PageTotalCountResponse.java
@@ -0,0 +1,10 @@
+package renewal.ektour.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PageTotalCountResponse {
+    private int totalCount;
+}
